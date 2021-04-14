fce86ffce86a3dc8927ddef452646b8ef90e1e2c
Akshay Kousal <akshay.kousal@gmail.com>
2021-04-14 20:50:26 +0530

    Log4J logging added

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -8,6 +8,8 @@ import com.aventstack.extentreports.markuputils.Markup;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
@@ -17,10 +19,13 @@ public class BaseTest {
 
     private static ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "\\reports\\Automation.html");;
     private static ExtentReports extent;
-    private static ExtentTest logger;
+    private static ExtentTest extentLogger;
+    private static Logger logger = Logger.getLogger("BaseTest");
 
     @BeforeTest
     public void beforeTest() {
+        PropertyConfigurator.configure("log4j.properties");
+        logger.info("***Test Start***");
         htmlReporter.config().setEncoding("utf-8");
         htmlReporter.config().setDocumentTitle("Automation-Report");
         htmlReporter.config().setReportName("Automation Test Results");
@@ -32,28 +37,31 @@ public class BaseTest {
 
     @BeforeMethod
     public void beforeMethod(Method method) {
-        logger = extent.createTest(method.getName());
+        logger.info("***Method " + method.getName() + " Start***");
+        extentLogger = extent.createTest(method.getName());
     }
 
     @AfterMethod
     public void afterMethod(ITestResult result) {
+        logger.info("***Method End***");
         if (result.getStatus() == ITestResult.SUCCESS) {
             String methodName = result.getMethod().getMethodName();
             String logText = "TEST CASE : " + methodName + " PASSED";
             Markup m = MarkupHelper.createLabel(logText, ExtentColor.GREEN);
-            logger.log(Status.PASS, m);
+            extentLogger.log(Status.PASS, m);
 
         } else if (result.getStatus() == ITestResult.FAILURE) {
             String methodName = result.getMethod().getMethodName();
             String logText = "TEST CASE : " + methodName + " FAILED";
             Markup m = MarkupHelper.createLabel(logText, ExtentColor.RED);
-            logger.log(Status.FAIL, m);
+            extentLogger.log(Status.FAIL, m);
         }
     }
 
 
     @AfterTest
     public void afterTest() {
+        logger.info("***Test End***");
         extent.flush();
     }
 
